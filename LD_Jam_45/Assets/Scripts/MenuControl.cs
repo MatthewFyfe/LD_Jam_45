47b416c13938a6e60dd13fdb847fa7a5bc60b0dd
Matthew Fyfe <mlf562@usask.ca>
2019-10-04 17:58:18 -0600

    Main menu with clicky buttons

---
+++ LD_Jam_45/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+	public Canvas mainMenuScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Unpause the game and continue
+    public void Continue()
+    {
+		Debug.Log("Continue");
+    }
+
+    // Reset the game to initial configuration
+    public void Restart()
+    {
+		Debug.Log("Restart");
+		SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    // Quit the game
+    public void Exit()
+    {
+    	Application.Quit();
+    	Debug.Log("Exiting game...");
+    }
+}
---
+++ LD_Jam_45/Assets/Scripts/mainGameLoop.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainGameLoop : MonoBehaviour
+{
+	public Canvas mainMenuScreen;
+	public AudioSource cameraMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	// Hide menu at start (for some reason hiding it in editor breaks stuff)
+        mainMenuScreen.enabled = false;
+        // Enable time and music if it was somehow disabled at scene start
+        Time.timeScale = 1;
+        cameraMusic.Play();
+
+    }
+
+    // Update is called once per frame (independent of timeScale)
+    void Update()
+    {
+  		if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+        }
+
+        // Toggle menu UI and pause / unpause game physics
+        if (Input.GetKeyDown("escape"))
+        {
+            mainMenuScreen.enabled = (mainMenuScreen.enabled == false);
+         	
+         	if(Time.timeScale == 1)
+         	{
+         		Time.timeScale = 0;
+         		cameraMusic.Pause();
+         	}
+         	else
+         	{
+         		Time.timeScale = 1;
+         		cameraMusic.Play();
+         	}
+        }
+    }
+
+    // Physics updates (frozen while timeScale == 0)
+    void FixedUpdate()
+    {
+
+    }
+
+}
