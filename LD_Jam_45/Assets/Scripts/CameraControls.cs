fa3dee4225ad0c2ac612e35331c4291991f30e56
Matthew Fyfe <mlf562@usask.ca>
2019-10-04 19:30:59 -0600

    Player movement and Camera

---
+++ LD_Jam_45/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+	Quaternion rotation;
+	public GameObject player;
+
+	public float cameraOffsetHeight = 5.0f;
+	public float cameraOffsetBack = 3.5f;
+
+	private Vector3 position;
+    
+    void Awake()
+    {
+    	
+    }
+
+    void LateUpdate()
+    {
+    	//Update camera to player position (and offset)
+    	position = player.transform.position;
+    	position.z -= cameraOffsetBack;
+    	position.y += cameraOffsetHeight;
+    	transform.position = position;
+    }
+}
---
+++ LD_Jam_45/Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour
+{
+	public float movementSpeed = 5;
+	public float rotationalSpeed = 50;
+
+	private Rigidbody rbody;
+	private float moveHorizontal, moveVertical;
+	private Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    	/* Do i want to use axis?
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        */
+
+        //Speed modifier
+    	if(Input.GetKey(KeyCode.LeftShift))
+    	{
+    		movementSpeed = 10;
+    		rotationalSpeed = 100;
+    	}
+    	else
+    	{
+    		movementSpeed = 5;
+    		rotationalSpeed = 50;
+    	}
+
+        // Handle Movement
+        if(Input.GetKey(KeyCode.W))
+        {
+        	transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed, Space.World);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+        	transform.Translate(Vector3.back * Time.deltaTime * movementSpeed, Space.World);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+        	transform.Translate(Vector3.left * Time.deltaTime * movementSpeed, Space.World);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+        	transform.Translate(Vector3.right * Time.deltaTime * movementSpeed, Space.World);
+        }
+
+         // Handle Rotation
+        if(Input.GetKey(KeyCode.Q))
+        {
+        	transform.Rotate(0,0,Time.deltaTime*rotationalSpeed*-1);
+        }
+        if(Input.GetKey(KeyCode.E))
+        {
+        	transform.Rotate(0,0,Time.deltaTime*rotationalSpeed*1);
+        }
+    }
+}
